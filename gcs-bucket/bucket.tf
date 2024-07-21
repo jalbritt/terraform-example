a0f7793754a07dab6178833f55e53a535999cd2f
James Albritton <jalbritt@users.noreply.github.com>
2024-07-21 12:34:12 +0200

    test tflint

--- gcs-bucket/bucket.tf
+++ gcs-bucket/bucket.tf
@@ -1,6 +1,6 @@
 # The first part of the resource signature "google_storage_bucket" comes from the google provider.
 resource "google_storage_bucket" "example-bucket" # The second part of the resource signature "example-bucket" is the local name of the resource. This is how you will refer to the resource in other parts of the configuration.
-  name          = "example-storage-bucket-jalbritt" # The name of the bucket. This must be globally unique.
+            = "example-storage-bucket-jalbritt" # The name of the bucket. This must be globally unique.
   location      = "US" 
   force_destroy = true
 
